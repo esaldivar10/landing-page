a22d583577c60e86a655e9e7325a345948e79421
Ezequiel <eze.saldivar@gmail.com>
2021-07-16 23:44:08 -0300

    Agregue bootstrap al trabajo

---
+++ src/App.js
@@ -0,0 +1,19 @@
+//import logo from './logo.svg';
+import Navbar from './componentes/navbar.js';
+import "bootstrap/dist/css/bootstrap.min.css"
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+        <h1>hola mundo</h1> 
+      </header>
+
+      <Navbar/>
+
+    </div> 
+  );
+}
+
+export default App;
---
+++ src/componentes/navbar.js
@@ -0,0 +1,8 @@
+const Navbar = () => {
+    return (
+    	<h1>Hola como andan</h1>
+
+    );
+}
+
+export default Navbar;
